f8777fc7f4cce7f9696bb58af10508db140a276e
KienHT <71423573+kien-ht@users.noreply.github.com>
2023-09-25 16:04:54 +0000

    ✨ refactor(utils-cjs): remove require

--- src/common/utils-cjs.ts
+++ src/common/utils-cjs.ts
@@ -1,6 +1,5 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import { createRequire } from 'module'
 
 export function __filename(importMetaUrl: string): string {
   return fileURLToPath(importMetaUrl)
@@ -8,8 +7,4 @@ export function __filename(importMetaUrl: string): string {
 
 export function __dirname(importMetaUrl: string): string {
   return dirname(__filename(importMetaUrl))
-}
-
-export function require(url: string, importMetaUrl: string) {
-  return createRequire(importMetaUrl)(url)
 }
