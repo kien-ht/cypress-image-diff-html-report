7f7ce997a22cdf03f6d2c02c5f06a7c0815bbabc
KienHT <71423573+kien-ht@users.noreply.github.com>
2023-12-17 10:36:52 +0000

    docs: 📚 add integration example for typescript project

---
+++ examples/typescript/cypress-image-diff.config.cjs
@@ -0,0 +1,6 @@
+const config = {
+  ROOT_DIR: '',
+  FAILURE_THRESHOLD: 0.09
+}
+
+module.exports = config
---
+++ examples/typescript/src/App.tsx
@@ -0,0 +1,16 @@
+import viteLogo from './vite.svg'
+
+function App() {
+  return (
+    <>
+      <img
+        src={viteLogo}
+        className="logo"
+        alt="Vite logo"
+      />
+      <h1>Cypress Image Diff Example - Typescript</h1>
+    </>
+  )
+}
+
+export default App
