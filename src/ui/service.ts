d5b1d84d8a6b45a0390ce05702d2820e3514d6b8
KienHT <71423573+kien-ht@users.noreply.github.com>
2024-05-06 12:07:38 +0000

    refactor: ✨ handle api request error

--- src/ui/service.ts
+++ src/ui/service.ts
@@ -5,6 +5,8 @@ export { getReports, updateTest }
 async function getReports(): Promise<ResolvedReport> {
   try {
     const response = await fetch('/api/reports')
+    if (!response.ok) throw Error('Network response was not OK')
+
     return await response.json()
   } catch (err) {
     throw Error((err as Error).message)
@@ -13,13 +15,14 @@ async function getReports(): Promise<ResolvedReport> {
 
 async function updateTest(testId: TestIdentity): Promise<void> {
   try {
-    await fetch('/api/reports', {
+    const response = await fetch('/api/reports', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(testId)
     })
+    if (!response.ok) throw Error('Network response was not OK')
   } catch (err) {
     throw Error((err as Error).message)
   }
